68425254ac805cce6ca3e80d9a82a62ebcdb59b1
DavHD09 <davidhdz99@hotmail.com>
2021-11-30 13:38:06 -0500

    Finalización formulario del login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,31 @@
 import '../css/Login.css'
+import {Link} from 'react-router-dom'
 
 function Login() {
     return (
         <div className="Form-Inicio mt-5">
             <img className="mt-4"src="/img/Res1.svg" alt="" id="logo-responsive"/>
-            <p>INICIAR SESIÓN</p>
+            <p id="Iniciar">INICIAR SESIÓN</p>
+            <form>
+            <input type="text" className="form-control ml-5 mr-5" id="Nombre" placeholder="Nombre"/>
+            <input type="password" className="form-control mx-5" id="Contraseña" placeholder="Contraseña"/>
+            </form>
+            <Link to=""><p id="Olvidar">¿Olvidaste tu contraseña?</p></Link>
+            <button class="btn btn-primary btnIngresar">Ingresar</button>
+            <Link to=""><p id="Registrate">Registrarse</p></Link>
+            <div className="redes">
+                <div className="row">
+                    <div className="col">
+                        <img id="logoface" src="/img/LogoFace.png" alt=""/>
+                    </div>
+                    <div className="col">
+                        <img id="logoinsta" src="/img/LogoInsta.png" alt=""/>
+                    </div>
+                    <div className="col">
+                        <img id="logowhat" src="/img/LogoWhat.png" alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
