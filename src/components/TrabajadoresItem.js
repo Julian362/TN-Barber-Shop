4d828cee131f7227e816078686cf8f25be7882b9
DavHD09 <davidhdz99@hotmail.com>
2021-12-05 12:05:15 -0500

    Agendar Cita

--- src/components/TrabajadoresItem.js
+++ src/components/TrabajadoresItem.js
@@ -2,13 +2,11 @@ import ModalAgendar from "./ModalAgendar";
 import '../css/Agenda.css'
 
 function TrabajadoresItem(props){
+
     return(
         <div className="row ListaTrabajadores">
-            <div className="prueba">
-                <button type="button" className="btn"  data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    <img src={props.rutafoto} alt="" /> 
-                    <ModalAgendar/>
-                </button>
+            <div className="prueba">                    
+                <ModalAgendar rutafoto={props.rutafoto}/>
                 <h5> {props.nickname} </h5>
             </div>                  
         </div>   
