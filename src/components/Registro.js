708d58388aaf2b8bf9cf35de835a11a247d3cb31
Julian362 <54406277+Julian362@users.noreply.github.com>
2021-12-13 10:46:02 -0500

    ok
    update registro con conexión a back

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -6,6 +6,40 @@ import { Link } from 'react-router-dom';
 
 function Registro() {
 
+    const showContraseña = function(e){
+            if(document.getElementById("Contraseñas-Registro").type === "text"){
+                document.getElementById("Contraseñas-Registro").type = "password";
+                document.getElementById("contraseña-show").value = "Mostrar";
+            }else if(document.getElementById("Contraseñas-Registro").type === "password"){
+                document.getElementById("Contraseñas-Registro").type = "text";
+                document.getElementById("contraseña-show").value = "Ocultar";
+            }
+    }
+    const showContraseña2 = function(e){
+            if(document.getElementById("Confirmar-Registro").type === "text"){
+                document.getElementById("Confirmar-Registro").type = "password";
+                document.getElementById("Confirmar-show").value = "Mostrar";
+            }else if(document.getElementById("Confirmar-Registro").type === "password"){
+                document.getElementById("Confirmar-Registro").type = "text";
+                document.getElementById("Confirmar-show").value = "Ocultar";
+            }
+    }
+    var host = "http://localhost:8081";
+    const consulta = function(e) {
+        const nombre = document.getElementById("Nombres-Registro").value;
+        console.log(nombre);
+        fetch(`${host}/producto/consultar/${nombre}`)
+            .then(res => res.json())
+            .then(res => {
+                document.getElementById("Apellidos-Registro").value = res.apellido;
+                document.getElementById("Documento-Registro").value = res.numero_documento;
+                document.getElementById("tipo-Documento-Registro").innerHTML = res.tipo_documento;
+                document.getElementById("Nickname-Registro").value = res.nickname;
+                document.getElementById("Correo-Registro").value = res.correo;
+                document.getElementById("Contraseñas-Registro").value = res.password;
+            })
+    }
+
     return (
         <div>
             <div className="espacio_para_que_no_se_vea_mal_el_icono_de_inicio">
@@ -23,8 +57,8 @@ function Registro() {
                     <input type="text" className="form-control input-Registro" id="Apellidos-Registro" placeholder="Apellidos" />
 
                     <div className="input-group input-Registro" style={{width:"85%"}}>
-                            <select class="form-select form-select-cc" style={{width:"25%"}}>
-                                <option selected>Tipo</option>
+                            <select className="form-select form-select-cc" style={{width:"25%"}}>
+                                <option selected id="tipo-Documento-Registro">Tipo</option>
                                 <option value="1">C.C</option>
                                 <option value="2">T.I</option>
                                 <option value="3">C.E</option>
@@ -34,9 +68,14 @@ function Registro() {
 
                     <input type="text" className="form-control input-Registro" id="Nickname-Registro" placeholder="Nickname" />
                     <input type="mail" className="form-control input-Registro" id="Correo-Registro" placeholder="Correo" />
-                    <input type="password" className="form-control input-Registro" id="Contraseñas-Registro" placeholder="Contraseña" />
-                    <input type="password" className="form-control input-Registro" id="Confirmar-Registro" placeholder="Confirmar contraseña" />
-
+                    <div className="input-group input-Registro" style={{width:"85%"}} >
+                        <input type="button" class="form-control form-icon" id="contraseña-show" onClick={showContraseña} style={{width:"25%"}} value="Mostrar"/>
+                        <input type="password" className="form-control input-Registro" id="Contraseñas-Registro" placeholder="Contraseña" style={{width:"75%"}} />
+                    </div>
+                    <div className="input-group input-Registro" style={{width:"85%"}} >
+                        <input type="button" class="form-control form-icon" id="Confirmar-show" onClick={showContraseña2} style={{width:"25%"}} value="Mostrar"/>
+                        <input type="password" className="form-control input-Registro" id="Confirmar-Registro" placeholder="Confirmar contraseña" style={{width:"75%"}} />
+                    </div>
                 </form>
 
                 <div className="custom-control custom-checkbox Politica-Registro">
@@ -46,7 +85,7 @@ function Registro() {
                     </label>
                 </div>
 
-                <button className="btn btn-primary btn-Registro ">Registrar</button>
+                <button className="btn btn-primary btn-Registro" onClick={consulta} >Registrar</button>
 
                 <Link className="Login-Registro" to='/Login' id="Login-Registro"><p>Ya tengo cuenta</p></Link>
                 <div className="Redes-Registro">
