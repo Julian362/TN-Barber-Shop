3c1e1413dbc963108cb7b81d36c65b6166719857
DavHD09 <davidhdz99@hotmail.com>
2021-12-18 21:55:32 -0500

    Conexión Gestion personal a base de datos (completo)

--- src/components/paginas/GestionPersonalAdmin.js
+++ src/components/paginas/GestionPersonalAdmin.js
@@ -7,10 +7,14 @@ import { useState, useEffect, Fragment } from 'react';
 
 
 const GestionPersonalAdmin = () => {
+
     const [listaEmpleados, setlistaEmpleados] = useState([]);
+    const [condicion,setcondicion]=useState(true);
+    const [empladoseleccionado,setempladoseleccionado] = useState({});
     var host = "http://localhost:8081";
+    
     useEffect(function () {
-        const solicitarEmpleados = () => {
+        const solicitarEmpleados = () => {            
             const rol = "usuario interno";
             fetch(`${host}/consultar/trabajadores/${rol}`)
             .then(res => res.json())
@@ -22,21 +26,29 @@ const GestionPersonalAdmin = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
+
     const cargarDatosInput = (nick,e) =>{
-        const variable_axu_trabajador = listaEmpleados.find((p) => p.nickname === nick);
+        var variable_axu_trabajador;
+        if(condicion){
+            variable_axu_trabajador = listaEmpleados.find((p) => p.nickname === nick);
+        }
+        else{
+            variable_axu_trabajador = empladoseleccionado;
+        }
         document.getElementById("nombre").value = variable_axu_trabajador.nombre;
         document.getElementById("apellido").value = variable_axu_trabajador.apellido;
         document.getElementById("Documento-Registro2").value = variable_axu_trabajador.numero_documento;
         document.getElementById("tipo-Documento-Registro2").innerHTML = variable_axu_trabajador.tipo_documento;
         document.getElementById("nickname").value = variable_axu_trabajador.nickname;
         document.getElementById("correo").value = variable_axu_trabajador.correo;
-        document.getElementById("cargo").value = variable_axu_trabajador.rol;
+        document.getElementById("rol").value = variable_axu_trabajador.rol;
         document.getElementById("contrasena").value = variable_axu_trabajador.password;
+        // document.getElementById("cargo").value = variable_axu_trabajador.cargo;
     }
-
     const empleados = listaEmpleados.map((p) =>
+                    
                     <>
-                        <div className="col" id={p.nickname} onClick={() => cargarDatosInput(p.nickname)}>
+                        <div className="col" id={p.nickname} style={{cursor:"pointer"}} onClick={() => cargarDatosInput(p.nickname) }>
                             <div className="card" style={{ width: "80%", marginLeft: "10%", border: 0 }}>
                                 <img src="/img/ImgTrabajador.png" className="card-img-top" alt="..." style={{ height: "fit-content", width: "auto" }} />
                                 <div className="card-body">
@@ -46,7 +58,62 @@ const GestionPersonalAdmin = () => {
                         </div>
                     </>
                 )
+    
+                      
+    function buscarempleado(){
+        const nicknamebuscar = document.getElementById("buscador").value;
+        fetch(`${host}/usuario/filtrar/${nicknamebuscar}`)
+        .then(res => res.json())            
+        .then(res => {
+            setempladoseleccionado(res);
+            setlistaEmpleados([]);
+            setcondicion(false);             
+            console.log(empladoseleccionado);
+        })
+    }
+        const empleadosfiltrados = (
+                        
+            <>
+                <div className="col" id={empladoseleccionado.nickname} style={{cursor:"pointer"}} onClick={() => cargarDatosInput(empladoseleccionado.nickname) }>
+                    <div className="card" style={{ width: "80%", marginLeft: "10%", border: 0 }}>
+                        <img src="/img/ImgTrabajador.png" className="card-img-top" alt="..." style={{ height: "fit-content", width: "auto" }} />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">{empladoseleccionado.nickname}</h5>
+                        </div>
+                    </div>
+                </div>
+            </> 
+    )
 
+    function RegistrosTotales(){
+        fetch(`${host}/usuarios/filtrar/registrostotales`)
+        .then(res => res.json())            
+        .then(res => {
+            setcondicion(true);
+            setlistaEmpleados(res);            
+        })
+        
+    }
+        
+    function filtrarTrabajadorPersonal(){
+        const rol = "usuario interno personal";
+        fetch(`${host}/usuario/filtrar/cargo/${rol}`)
+        .then(res => res.json())            
+        .then(res => {
+            setcondicion(true);
+            setlistaEmpleados(res);            
+        })
+    }
+
+    function filtrarSupervisores(){
+        const rol = "usuario interno super";
+        fetch(`${host}/usuario/filtrar/cargo/${rol}`)
+        .then(res => res.json())            
+        .then(res => {
+            setcondicion(true);
+            setlistaEmpleados(res);            
+        })
+    }
     return (
         <>
             <div className="bg-white" style={{ height: "120px" }}></div>
@@ -57,17 +124,27 @@ const GestionPersonalAdmin = () => {
                     style={{ width: "25%", height: "fit-content", marginLeft: "37.5%" }} />
 
                 <p className="text-center mt-3" style={{ fontSize: "36px" }}>ADMINISTRADOR</p>
+                
+                {/* <div>
+                    <h4 style={{textAlign:"center"}}> Editar datos de los trabajadores seleccionados. </h4>
+                </div> */}
 
-
-                <div className="btn-group col-6 m-auto" role="group" aria-label="Basic radio toggle button group">
-                    <div>
-                        <input type="radio" className="btn-check btn-primary btnradio1" name="btnradio" id="btnradio1" autoComplete="off" defaultChecked style={{ backgroundColor: "red" }} />
-                        <label className="btn btn-outline-secondary" htmlFor="btnradio1">Personal</label>
+                <div className="btn-group col-8 m-auto primary" role="group" aria-label="Basic radio toggle button group">
+                   
+                    <div style={{ width: "50%" }}>
+                        <input type="radio" className="btn-check" name="btnradio" onClick={RegistrosTotales} id="btnradio2" autoComplete="off" defaultChecked />
+                        <label className="btn btn-outline-secondary" htmlFor="btnradio2" style={{ width: "100%" , height:"90%", fontSize:"22px", paddingTop:"2px"}} > todos los registros </label>
                     </div>
 
-                    <div>
-                        <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" />
-                        <label className="btn btn-outline-secondary" htmlFor="btnradio3">Supervisor</label>
+
+                    <div style={{ width: "50%" }}>
+                        <input type="radio" className="btn-check btn-primary btnradio1" onClick={filtrarTrabajadorPersonal} name="btnradio" id="btnradio1" autoComplete="off"/>
+                        <label className="btn btn-outline-secondary" htmlFor="btnradio1" style={{ width: "100%" , height:"90%", fontSize:"22px", paddingTop:"2px"}} > Barberos </label>
+                    </div>
+
+                    <div style={{ width: "50%" }}>
+                        <input type="radio" className="btn-check" name="btnradio" onClick={filtrarSupervisores} id="btnradio3" autoComplete="off" />
+                        <label className="btn btn-outline-secondary" htmlFor="btnradio3" style={{ width: "100%" , height:"90%", fontSize:"22px", paddingTop:"2px"}}> Supervisores </label>
                     </div>
 
                 </div>
@@ -75,16 +152,32 @@ const GestionPersonalAdmin = () => {
                 <FormGestionPersonal/>
 
                 <div className="input-group mb-3 m-auto col-6">
-                    <input type="text" className="form-control text-center " placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" style={{ height: "fit-content", width: "auto", paddingLeft: "15%" }} />
-                    <span className="input-group-text bi bi-search" id="basic-addon1"></span>
+                    <input type="text" className="form-control text-center" id="buscador" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" style={{ height: "fit-content", width: "auto", paddingLeft: "15%" }} />
+                    <span className="input-group-text bi bi-search" onClick={buscarempleado} id="basic-addon1" style={{ cursor:"pointer" }}></span>
                 </div>
 
-                <div className="container mt-4 mb-4">
-                    <div className="row row-cols-6">
+                {condicion ?                
+                    <div className="container mt-4 mb-4" style={{textAlign:"center"}}>
+                        <div className="row row-cols-6" style={{textAlign:"center"}}> 
+                            {empleados}
+                        </div>
+                    </div>
+                : 
+                    <div className="container mt-4 mb-4">
+                        <div className="row row-cols-6"> 
+                            {empleadosfiltrados}
+                        </div>
+                    </div>
+                
+                }
+
+                {/* <div className="container mt-4 mb-4">
+                    <div className="row row-cols-6"> 
                         {empleados}
                     </div>
                 </div>
-
+                empleadosfiltrados
+ */}
                 <div style={{ height: "80px" }}><Redes /></div>
             </div>
 
